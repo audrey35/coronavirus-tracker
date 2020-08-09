3efc8e3de1a616f16c6cdc3087a77998a6c43914
Audrey Jo <audreyjo35@gmail.com>
2020-08-09 00:47:46 -0400

    remove Java 11 usage

--- src/main/java/com/audrey/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/audrey/coronavirustracker/services/CoronaVirusDataService.java
@@ -7,12 +7,11 @@ import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,17 +39,12 @@ public class CoronaVirusDataService {
     @Scheduled(cron="* * 1 * * *") // Schedules a method to run on a regular basis.
     // Set to run every second cron="* * * * * *"
     // Set to run 1st hour of every day cron="* * 1 * * *"
-    public void fetchCovidData() throws IOException, InterruptedException {
+    public void fetchCovidData() throws Exception {
         List<LocationStats> newStats = new ArrayList<>();
-        HttpClient client = HttpClient.newHttpClient(); // create a client
-        HttpRequest request = HttpRequest.newBuilder() // create a request to be made on covid url
-                .uri(URI.create(COVID_DATA_URL))
-                .build();
-        HttpResponse<String> httpResponse; // response returned when client sends the request
-        httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         // Source: https://commons.apache.org/proper/commons-csv/user-guide.html
-        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        String csvBodyString = getHTML(COVID_DATA_URL);
+        StringReader csvBodyReader = new StringReader(csvBodyString);
         Iterable<CSVRecord> records;
         records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
@@ -64,5 +58,26 @@ public class CoronaVirusDataService {
             newStats.add(locationStat);
         }
         this.allStats = newStats;
+    }
+
+    /**
+     * Streams a web page and returns a string of the response.
+     * source: https://stackoverflow.com/a/1485730
+     * @param urlToRead URL of the web page.
+     * @return String representation of the web page's response.
+     * @throws Exception
+     */
+    public String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
     }
 }
